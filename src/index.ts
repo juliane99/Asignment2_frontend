4e823fefa04e746b2507006f54d9e5dccc5ee5f1
Juliane <57350721+juliane99@users.noreply.github.com>
2020-04-18 14:57:09 +0200

    fixed another bugs in ts
    -changed const into var
    - added !
    - added
    -  `${}`

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@
 function calculateTip() {
     const amountofBill = document.querySelector<HTMLInputElement>("#bill")!.value; //Billamount
     const service = document.querySelector<HTMLInputElement>("#service")!.value; // Percentage of Service
-    const sharedByPeople = document.querySelector<HTMLInputElement>("#people")!.value; //Number of People
+    var sharedByPeople = document.querySelector<HTMLInputElement>("#people")!.value; //Number of People
   
     //check input -> if user wrote sometinh in input field or choosed an option  
-    if (amountofBill === "" || service  == 0) {
+    if (amountofBill === "" || service  == `${0}`) {
       alert("Please enter values");
       return;
     }
     //Check to see if this input is empty or less than or equal to 1
-    if (sharedByPeople === "" || sharedByPeople <= 1) {
-      sharedByPeople = 1;
-      document.getElementById("each").style.display = "none";
+    if (sharedByPeople === "" || sharedByPeople <= `${1}`) {
+      sharedByPeople =  `${1}`;
+      document.getElementById("each")!.style.display = "none";
     } else {
-      document.getElementById("each").style.display = "block";
+      document.getElementById("each")!.style.display = "block";
     }
   
     //Calculate the actual tip
@@ -24,17 +24,21 @@ function calculateTip() {
     //next line allows us to always have two digits after decimal point
     total = total.toFixed(2);
     //Show the tip 
-    document.getElementById("totalTip").style.display = "block";
-    document.getElementById("tip").innerHTML = total;
+    document.getElementById("totalTip")!.style.display = "block";
+    document.querySelector<HTMLElement>("#tip")!.innerHTML = `${total}`;
   
   }
   
   //Hide the tip amount on load
-  document.getElementById("totalTip").style.display = "none";
-  document.getElementById("each").style.display = "none";
+  document.getElementById("totalTip")!.style.display = "none";
+  document.getElementById("each")!.style.display = "none";
   
   //click to call function
-  document.getElementById("calculate").onclick = function() {
+  document.getElementById("calculate")!.onclick = function() {
     calculateTip();
   
   };
+  //const button = document.querySelector<HTMLElement>('#calculate');
+//button!.onclick = () => {
+  //calculateTip();
+//}
