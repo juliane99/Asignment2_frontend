4958e94f1d1c47cc9761adf6a9555bae55831059
Juliane <57350721+juliane99@users.noreply.github.com>
2020-04-18 14:40:18 +0200

    deleted

--- Tip_Cost_Calculator/TypeScriptAssignment-master/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { helloWorld, Beispiel } from './myModule';
-import { alertMe } from './myOtherModule';
-
-console.log(helloWorld);
-customElements.define('my-beispiel', Beispiel)
-
-alertMe();
--- Tip_Cost_Calculator/TypeScriptAssignment-master/src/myModule.ts
+++
@@ -1,8 +0,0 @@
-export const helloWorld: string = "Hello world";
-
-export class Beispiel extends HTMLElement{
-    constructor(){
-        super();
-        this.innerHTML = "<h1>Hello World</h1>";
-    }
-}
