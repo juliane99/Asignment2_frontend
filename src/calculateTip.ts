26f5543a1120979860ba84f433e6a02d1acd48f5
Juliane <57350721+juliane99@users.noreply.github.com>
2020-04-19 21:13:44 +0200

    added another ts file + validate sharedbypeople field
    - added ts file "calculateTip" with the Calculate Tip function
    - importet it to index.ts (const button gets the function)
    - validate sharedbyPeople -> have to write a number in it.

---
+++ src/calculateTip.ts
@@ -0,0 +1,31 @@
+//Calculate the Tip
+export function calculateTip() {
+    const amountofBill = document.querySelector<HTMLInputElement>("#bill")!.value; //Billamount
+    const service = document.querySelector<HTMLInputElement>("#service")!.value; // Percentage of Service
+    var sharedByPeople = document.querySelector<HTMLInputElement>("#people")!.value; //Number of People
+
+  
+    //check input -> if user wrote sometinh in input field or choosed an option  
+    if (amountofBill === "" || service  == `${0}` || sharedByPeople === "") {
+      alert("Please enter values");
+      return;
+    }
+    //Check to see if this input is empty or less than or equal to 1
+    if (sharedByPeople === "" || sharedByPeople <= `${1}`) {
+      sharedByPeople =  `${1}`;
+      document.getElementById("each")!.style.display = "none";
+    } else {
+      document.getElementById("each")!.style.display = "block";
+    }
+    
+    //Calculate the actual tip
+    var total = (<any>amountofBill * <any>service) / <any>sharedByPeople;
+    //round to two decimal places
+    total = Math.round(total * 100) / 100;
+    //next line allows us to always have two digits after decimal point
+    total = parseFloat(total.toFixed(2));  
+    //Show the tip 
+    document.getElementById("totalTip")!.style.display = "block";
+    document.querySelector<HTMLElement>("#tip")!.innerHTML = `${total}`;
+  
+  }
