2ca296d9e98486b8911ef4feddc5e862723604af
Juliane <57350721+juliane99@users.noreply.github.com>
2020-04-24 12:08:45 +0200

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
   import { onload } from './onload';
   import { calculateTip } from './calculateTip';
   import {hideContainer} from './hideTheContainer';
+  import {popup} from './popUp';
 
 onload();
 
@@ -14,29 +15,4 @@ button2!.onclick = () => {
  hideContainer();
 }
 
-function popup() {
-  const Popupbutton = document.querySelector("#popupButton");
-  const allButtons = document.querySelector(".button-small");
-  const popupButton = document.querySelector(".button-popup");
-  const container = document.querySelector("#container");
-  const PopupClose = document.querySelector("#closePopup");
-  
-  Popupbutton!.addEventListener("click", displayPopUp);
-  PopupClose!.addEventListener("click", closePopUp);
-  
-  function displayPopUp(){
-      allButtons!.classList.add("blur");
-      popupButton!.classList.add("blur");
-      container!.classList.add("blur");
-      document.getElementById("popup")!.style.display = "block";
-  }
-  
-  function closePopUp(){
-      allButtons!.classList.remove("blur");
-      popupButton!.classList.remove("blur");
-      container!.classList.remove("blur");
-      document.getElementById("popup")!.style.display = "none";
-  }
-  
-  }
-  popup();
+popup();
